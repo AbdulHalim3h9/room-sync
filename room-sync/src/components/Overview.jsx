631abae1f395d8e8c8af07b2d2dc1c00986338b9
AbdulHalim3h9 <abdulhalimkhan271@gmail.com>
2025-01-08 00:31:19 +0600

    chart positioned

--- room-sync/src/components/Overview.jsx
+++ room-sync/src/components/Overview.jsx
@@ -11,6 +11,7 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
+import PieContribution from "./PieContribution";
 
 const data = [
   { name: "Abdul Halim", uv: 4000, pv: 2400, amt: 2400 },
@@ -130,23 +131,8 @@ export default function Overview() {
         </LineChart>
       </div>
       {/* pie chart */}
+<PieContribution />
 
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={false}
-            data={data01}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          />
-          <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
     </div>
   );
 }

b0d99041337e3ab74c91218ae6561f12b6fcda36
AbdulHalim3h9 <abdulhalimkhan271@gmail.com>
2025-01-12 23:31:52 +0600

    Added recipet styling

--- room-sync/src/components/Payables.jsx
+++ room-sync/src/components/Payables.jsx
@@ -18,7 +18,7 @@ const Payables = () => {
   return (
     <div className="container mx-auto p-6 grid grid-cols-1 sm:gap-2 md:gap-4 gap-y-4 sm:grid-cols-2 lg:grid-cols-3">
       {bills.map((bill) => (
-        <Card key={bill.id} className="flex">
+        <Card key={bill.id} className="flex bg-slate-50 shadow-xl"> {/* Add shadow to the Card */}
           <div className="flex flex-col items-center w-full"> {/* Center content horizontally */}
             <CardHeader>
               <CardTitle>
@@ -27,14 +27,14 @@ const Payables = () => {
             </CardHeader>
             
             <CardContent className="w-full flex flex-col items-center"> {/* Center content within CardContent */}
-            <hr className="w-full border-gray-300 my-2" /> 
+            <hr className="w-full border-gray-300 border-dashed my-2" /> 
               {bill.payables.map((payable, index) => (
                 <div key={index} className="flex justify-between w-full max-w-[300px]">
                   <span className="px-4">{payable.name}:</span>
                   <span>{payable.amount}tk</span>
                 </div>
               ))}
-              <hr className="w-full border-gray-300 my-2" /> 
+              <hr className="w-full border-gray-300 border-dashed my-2" /> 
               <div className="flex justify-between w-full max-w-[300px] py-2">
                 <span className="px-4">**Total:</span>
                 <span>
